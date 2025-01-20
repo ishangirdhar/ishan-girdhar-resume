ac81224085662f9e719f5505635e114716aa610f
Ishan Girdhar <ishan@alphaciso.com>
2025-01-21 00:46:38 +0800

    Refactor global styles and layout for improved print and visual consistency
    - Adjusted padding, margins, and gaps in globals.css to enhance print layout and overall spacing.
    - Updated main content and grid styles for better responsiveness and reduced spacing.
    - Implemented new styles for page margins and ensured proper handling of page breaks.
    - Modified the Resume component layout in page.tsx to align with the updated styles, improving visual presentation.
    
    Signed-off-by: Ishan Girdhar <ishan@alphaciso.com>

--- app/page.tsx
+++ app/page.tsx
@@ -10,11 +10,11 @@ import resumeData from './data/resume.json';
 
 export default function Resume() {
   return (
-    <div className="bg-zinc-300 p-8">
+    <div className="bg-zinc-300">
       <div className="shadow-3xl mx-auto max-w-7xl bg-white">
         <Header />
 
-        <main className="p-6">
+        <main className="px-2 py-2">
           <div className="grid grid-cols-3 gap-6">
             {/* Work Experience Section */}
             <section className="col-span-2">
